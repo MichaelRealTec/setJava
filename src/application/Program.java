ec7fa560ddcdd655d07c18b3ec7c7c60d36e31a3
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-10-04 22:47:39 -0300

    teste com HashSet

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,24 @@
 package application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// set<T> é um tipo genérico ( Representa um conjunto de elementos (similar ao da Álgebra)
+		// set é uma interface na verdade e tem umas implementações = 1º HashSet (+rápido) 2º TreeSet (+lento) 3º LinkedHashSet (intermediario)
+		Set<String> set = new HashSet<>();
+		
+		set.add("TV");
+		set.add("Notebook");
+		set.add("Tablet");
+		
+		System.out.println(set.contains("Notebook"));
+		
+		for(String p : set) {
+			System.out.println(p);
+		}
 		
 	}
 
