528df0094b20f1124df43c90d6d5c475f759df09
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-10-04 22:55:54 -0300

    remove - tablet -

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,23 @@
 package application;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// set<T> é um tipo genérico ( Representa um conjunto de elementos (similar ao da Álgebra)
 		// set é uma interface na verdade e tem umas implementações = 1º HashSet (+rápido) 2º TreeSet (+lento) 3º LinkedHashSet (intermediario)
-		Set<String> set = new TreeSet<>();
+		Set<String> set = new LinkedHashSet<>();
+		// HashSet não mantem a ordem dos arquivos
 		// TreeSet ordena os dados 
+		//LinkedHashSet ordena os arquivos
 		
 		set.add("Tv"); // Segunda letra maiuscula tem prioridade
 		set.add("Tablet"); // Observação troquei a ordem e o resultado mudou
 		set.add("Notebook");
+		
+		set.remove("Tablet"); // Observação: Nome tem que ser igual para remover
 
 		
 		System.out.println(set.contains("Notebook"));
