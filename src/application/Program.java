415a2c53587ab79030a903ebc63647e5cd7d2be0
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-10-04 22:41:19 -0300

    primeiro commit

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// set<T> é um tipo genérico ( Representa um conjunto de elementos (similar ao da Álgebra)
+		
+	}
+
+}
